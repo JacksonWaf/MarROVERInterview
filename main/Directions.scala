b397b196af1cedc451e6e20354ab55d2b1842efc
JacksonWaf <jackson7wafula@gmail.com>
2019-07-18 19:26:21 +0300

    Update Directions.scala

--- main/Directions.scala
+++ main/Directions.scala
@@ -7,5 +7,14 @@
 sealed abstract class Directions (currentDirection: String,
                                 rightDirection: String,
                                 leftDirection: String) {
+def current() = currentDirection
+	def right() = rightDirection
+	def left() = leftDirection
+                                }
+                                case object North extends Direction("N", "E", "W")
+case object South extends Direction("S", "W", "E")
+case object East extends Direction("E", "S", "N")
+case object West extends Direction("W", "N", "S")
+
 
 }
