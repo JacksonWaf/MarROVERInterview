7149968a8d2db8481fac6cc4cc5df54267e093ff
JacksonWaf <jackson7wafula@gmail.com>
2019-07-18 19:54:01 +0300

    Update Grid.scala

--- main/Grid.scala
+++ main/Grid.scala
@@ -8,6 +8,9 @@ case class Grid (x: Int, y: Int) {
   defPositionOfNextPoint(position: Position) : Position = position match
   {
     case Position(px, py, North) => position(px, moveNorth(py), North)
+      case Position(px, py, East) => position(px, moveEast(py), East)
+        case Position(px, py, South) => position(px, moveSouth(py), South)
+          case Position(px, py, West) => position(px, moveWest(py), West)
     
   }
 }
